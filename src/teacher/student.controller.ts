21d119250bb2d8c0a17cfd4bb76b27db603ecb7d
Seung-wan <tmddhks0104@gmail.com>
2022-01-02 20:54:01 +0900

    feat. add pipe

--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Put } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Put } from '@nestjs/common';
 import { StudentService } from '../student/student.service';
 import {
   FindStudentResponseDto,
@@ -9,14 +9,16 @@ export class StudentTeacherController {
   constructor(private readonly studentService: StudentService) {}
 
   @Get()
-  getStudents(@Param('teacherId') teacherId: string): FindStudentResponseDto[] {
+  getStudents(
+    @Param('teacherId', new ParseUUIDPipe()) teacherId: string,
+  ): FindStudentResponseDto[] {
     return this.studentService.getStudentsByTeacherId(teacherId);
   }
 
   @Put('/:studentId')
   updateStudentTeacher(
-    @Param('teacherId') teacherId: string,
-    @Param('studentId') studentId: string,
+    @Param('teacherId', new ParseUUIDPipe()) teacherId: string,
+    @Param('studentId', new ParseUUIDPipe()) studentId: string,
   ): StudentResponseDto {
     return this.studentService.updateStudentTeacher(teacherId, studentId);
   }
